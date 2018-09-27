6c9c762e0f1e0d7ee78d568fece69e9fb79f211d
ssdemogit <demossgit@gmail.com>
2018-09-27 17:09:05 +0530

    brigade

--- brigade.js
+++ brigade.js
@@ -3,27 +3,29 @@ const { events, Job, Group } = require("brigadier");
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.commit)
 
-  var dockerBuild = new Job("docker-build")
-
+var dockerBuild = new Job("docker-build")
+ 
   dockerBuild.image = "docker:dind"
   dockerBuild.privileged = true; // dind needs to run in privileged mode
-  dockerBuild.storage.enabled = false;
+ 
   dockerBuild.env = {
-   //DOCKER_DRIVER: "overlay"
-    "DOCKER_DRIVER": "overlay",
-    "dockerBuild.env.DOCKER_USER": "project.secrets.dockerLogin",
-    "dockerBuild.env.DOCKER_PASS": "project.secrets.dockerPass",
-    //"Storage Driver": "overlay"
- }
-
-    dockerBuild.tasks = [
-    "dockerd-entrypoint.sh &", // Start the docker daemon
-    "sleep 30", // Grant it enough time to be up and running
+    DOCKER_DRIVER: "overlay"
+  }
+ 
+  // Place these credentials in your project YAML and update it using helm 
+  dockerBuild.env.DOCKER_USER = project.secrets.dockerLogin 
+  dockerBuild.env.DOCKER_PASS = project.secrets.dockerPass
+ 
+  dockerBuild.tasks = [
+    "dockerd-entrypoint.sh &amp;", // Start the docker daemon
+    "sleep 20", // Grant it enough time to be up and running
     "cd /src/", // Go to the project checkout dir
-    "docker build -t html:v4 .", // Replace with your own image tag
+    "docker build -t nimbus2005/html .", // Replace with your own image tag
     "docker login -u $DOCKER_USER -p $DOCKER_PASS",
-    "docker push nimbus2005/html:v4", // Replace with your own image tag
+    "docker push nimbus2005/html:v4" // Replace with your own image tag
   ]
-
- dockerBuild.run()
+ 
+  dockerBuild.run().then( () =&gt; {
+    events.emit("build-done", e, project) // Fire the next event
+  })
 })
