8c0b03ae00519a960ced5c8a19bdc96cd393fa50
ssdemogit <demossgit@gmail.com>
2018-09-27 18:55:05 +0530

    brigade

--- brigade.js
+++ brigade.js
@@ -10,7 +10,7 @@ var dockerBuild = new Job("docker-build")
   package.storage.enabled = false; 
   
   dockerBuild.env = {
-    DOCKER_DRIVER: "overlay",
+    "DOCKER_DRIVER": "overlay",
   // Place these credentials in your project YAML and update it using helm 
   "DOCKER_USER": "project.secrets.dockerLogin", 
   "DOCKER_PASS": "project.secrets.dockerPass",
