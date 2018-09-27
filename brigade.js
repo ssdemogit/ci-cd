7c39ba82a57f6f23e0370ad2bb7ea7bcb24f979b
ssdemogit <demossgit@gmail.com>
2018-09-27 15:25:48 +0530

    brigade

--- brigade.js
+++ brigade.js
@@ -14,7 +14,6 @@ events.on("push", function(e, project) {
     "dockerBuild.env.DOCKER_USER": "project.secrets.dockerLogin",
     "dockerBuild.env.DOCKER_PASS": "project.secrets.dockerPass",
     "Storage Driver": "overlay"
-    "Backing Filesystem": "xfs"
     "Supports d_type": "false"
  }
 
