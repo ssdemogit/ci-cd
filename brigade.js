687762a20b2391b94c6eb3b6be79d33a27a83f39
ssdemogit <demossgit@gmail.com>
2018-09-27 11:51:19 +0530

    brigade

--- brigade.js
+++ brigade.js
@@ -3,20 +3,20 @@ const { events, Job } = require("brigadier");
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.commit)
 
-// build docker
+events.on("test-done", (e, project) =&gt; {
 var dockerBuild = new Job("docker-build")
- 
+
   dockerBuild.image = "docker:dind"
   dockerBuild.privileged = true; // dind needs to run in privileged mode
- 
+
   dockerBuild.env = {
     DOCKER_DRIVER: "overlay"
   }
- 
+
   // Place these credentials in your project YAML and update it using helm 
   dockerBuild.env.DOCKER_USER = project.secrets.dockerLogin 
   dockerBuild.env.DOCKER_PASS = project.secrets.dockerPass
- 
+
   dockerBuild.tasks = [
     "dockerd-entrypoint.sh &amp;", // Start the docker daemon
     "sleep 20", // Grant it enough time to be up and running
@@ -25,7 +25,7 @@ var dockerBuild = new Job("docker-build")
     "docker login -u $DOCKER_USER -p $DOCKER_PASS",
     "docker push nimbus2005/html:v4", // Replace with your own image tag
   ]
- 
+
   dockerBuild.run()
 })
 
