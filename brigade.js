c0155c36a64781cac7fe910dd18154217c8ca12a
ssdemogit <37206861+ssdemogit@users.noreply.github.com>
2018-10-10 14:28:04 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,6 +1,8 @@
-const { events, Job, Group } = require("brigadier");
+const { events, Job, Group } = require('brigadier')
+
 events.on("push", (brigadeEvent, project) => {
-//events.on("push", (e, project) => {
+
+  //events.on("push", (e, project) => {
   //console.log("received push for commit " + e.commit)
   var azClientSecret = project.secrets.appId
  var azTenant = project.secrets.tenant
